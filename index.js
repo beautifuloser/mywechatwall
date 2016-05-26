4bda56b17419114aede9afa35c944f39af44b812
joy <466952688@182.92.8.177>
2016-05-26 18:15:56 +0800

    get message from wechat
    get message from wechat

--- index.js
+++ index.js
@@ -1,30 +1,53 @@
 var wxPort = require('./lib/config').wxPort;
+var token = require('./lib/config').token;
 var express = require('express');
-var checkSignature = require('./utils/checkSignatureUtil');
+var qs = require('qs');
+var url = require('url');
+var http = require('http');
+var xml2js = require('xml2js');
+var checkSignature = require('./utils/checkSignatureUtil').checkSignature;
+
 var app = express();
-
-//设置静态文件路径
-app.use(express.static(__dirname + '/client'));
-
-app.use(function(req,res){
-	console.log("1111");
-	console.log(req.query);
-	if (req.method == 'GET') {
-		console.log("222");		
-		//get方法
-		if (req.body.signature) {
-			//微信验证
-			
+//验证微信服务器
+app.use(function(req,res,next){
+	var query = url.parse(req.url).query;
+	var params = qs.parse(query);
+	if (req.method == 'GET'&&params.signature) {
+		if (!checkSignature(params,token)) {
+			return false;
 		}else{
-			console.log("333");
-			//网页接入			
-			res.sendFile(__dirname + '/client/index.html');	
+			res.end(params.echostr); 
 		}
 	}else{
-		//post方法
+		next();
 	}
 });
-
-app.listen(wxPort,function(){
-	console.log('app is running at port : ' + wxPort);
+//设置静态文件路径
+app.use(express.static(__dirname + '/client'));
+app.use(function(req,res){
+	if (req.method == 'GET') {
+		res.sendFile(__dirname + '/client/index.html');	
+	}else{
+		var query = url.parse(req.url).query;
+		var params = qs.parse(query);
+		//验证消息是否来自微信服务器
+		if (checkSignature(params,token)) {
+			var postData = "";
+			req.addListener("data",function(chunk){
+				postData += chunk;
+			});
+			req.addListener("end",function(){
+				var parseString = xml2js.parseString;
+				parseString(postData,function(err,result){
+					if (!err) {
+						
+					};
+				});
+			});
+		};
+	};
 });
+
+var server = http.createServer(app);
+server.listen(wxPort);
+console.log("the server is listen at port :"+wxPort);
--- utils/checkSignatureUtil.js
+++ utils/checkSignatureUtil.js
@@ -1,3 +1,11 @@
-exports.checkSignature = function (argument) {
-	
+var crypto = require('crypto');
+function checkSignature(params,token) {
+	var key = [token,params.timestamp,params.nonce].sort().join('');
+	var sha1 = crypto.createHash('sha1');
+	sha1.update(key);
+	return sha1.digest('hex') == params.signature;
+};
+
+module.exports = {
+	checkSignature : checkSignature
 }
